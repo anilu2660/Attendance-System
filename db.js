0f40f9ee10b79ced7ad5f3c535b03b49aa063334
anilu2660 <upadhyayanuj526@gmail.com>
2025-11-26 20:10:20 +0530

    new files

--- db.js
+++ db.js
@@ -1,13 +1,13 @@
 // db.js
-const mysql = require('mysql2/promise');
 require('dotenv').config();
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: process.env.DB_HOST ,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.PORT || 3306,
+  port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const pool = require('./db'); 
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.APP_PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
