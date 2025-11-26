0b470fc401d034aa62f59cd86a2fedd58eb147b1
anilu2660 <upadhyayanuj526@gmail.com>
2025-11-26 19:23:05 +0530

    new files

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST || 'http://student-attendance-system-0.cfwiwmgememr.ap-south-1.rds.amazonaws.com',
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
